aef59acc20f3d067afbd9e161ffd5129583b280a
Tomoaki Imai <tomoima525@gmail.com>
2021-10-02 15:53:00 -0700

    Add Wallet provider component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import dynamic from "next/dynamic";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const WalletConnectionProvider = dynamic(() => import("../components/Wallet"), {
+  ssr: false,
+});
+
+function HayamaApp({ Component, pageProps }: AppProps) {
+  const localAddress = "http://localhost:8899";
+  return (
+    <WalletConnectionProvider
+      network={WalletAdapterNetwork.Devnet}
+      localAddress={localAddress}
+    >
+      <Component {...pageProps} />
+    </WalletConnectionProvider>
+  );
 }
-export default MyApp
+export default HayamaApp;
